e06934d63983fcedb696c63d460660b23955abf8
Gayatri_Butkar <gayatributkar14@gmail.com>
2025-02-25 12:51:30 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,24 @@
+// console.log("Heelo0")
+
+//
+//Number
+let score=102
+// String 
+let name="Shreeya";
+//Boolean
+let isvalid=false;
+//Objects
+let teatypes=["lemon tea","orange tea","oolong tea"];
+let user={firstname  :"Shreeya" , Lastname :"Butkar"};
+
+console.log(score);
+console.log(name);
+console.log(isvalid);
+console.log(teatypes);
+console.log(user);
+
+
+
+
+
+
