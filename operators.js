e62df6184770e74ef597e81e7a1722571e7a60da
Gayatri_Butkar <gayatributkar14@gmail.com>
2025-02-25 12:51:52 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,82 @@
+// Arithmetic operators
+ let score= 3;
+let bonus=5;
+totalscore=score +bonus;
+
+let addition =4 + 5;
+let subtraction = 8 - 4;
+let mult = 6*8;
+let div=8/2;
+let remainder = 6 % 4;
+let expo = 2**4;
+
+let myscore=110;
+myscore++;
+
+let credits=450;
+credits--;
+
+console.log(addition);
+console.log(subtraction);
+console.log(mult);
+console.log(div);
+console.log(expo);
+console.log(myscore);
+console.log(credits);
+
+// Comparison Operators
+
+let a = 6
+let b = 8
+let c = 9
+
+console.log(a == b );
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+console.log();
+
+
+// Logical Operators
+
+// && - AND
+// || - OR
+// ! - NOT
+
+let isLoggedin = true;
+let ispaid = true;
+
+// if both the conditions  are true then AND operator returns true
+console.log(isLoggedin && ispaid);
+
+isgoogleuser = true;
+isLoggedin = false;
+// Returns true if either of the condition is true
+console.log(isgoogleuser || isLoggedin)
+
+//Simply converts the true value to false and vice versa
+console.log(! isLoggedin)
+
+console.log()
+// ASSIGNMENT OPERATORS
+ a=22;
+ console.log(a);
+ a+=5
+ console.log(a);
+ a-=4
+ console.log(a);
+ a/=2
+ console.log(a);
+ a*=5
+ console.log(a);
+ a**=5
+ console.log(a);
+
+
+
+
+
+
