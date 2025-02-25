3a0336f489cf73d0b593d9c86e009b6dfa97f70b
Gayatri_Butkar <gayatributkar14@gmail.com>
2025-02-25 12:52:40 +0530

    Add files via upload

---
+++ nonprimitive.js
@@ -0,0 +1,26 @@
+let user_name={
+    "first name" :"Rohit",
+    isLoggedin :true,
+ 
+};
+console.log(user_name);
+console.log(user_name['first name']);
+console.log(user_name.isLoggedin);
+
+console.log(typeof user_name);
+user_name.firstname="Mr. H"
+user_name.lastName="Chaudhary"
+console.log(user_name);
+
+console.log()
+let today =new Date()
+console.log(today.getDate());
+
+console.log()
+//Array - 
+
+let another_user = ["hitesh" ,true]
+console.log(another_user[0]);
+
+let isvalue=true;
+console.log(isvalue + 1);
