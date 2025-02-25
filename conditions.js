9716ff8a1065843a3183968ba2926b659cb31173
Gayatri_Butkar <gayatributkar14@gmail.com>
2025-02-25 12:53:34 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,64 @@
+let num1=5;
+let num2=8;
+
+if (num1 > num2) 
+{
+    console.log("num 1 is greater than num2");
+    
+}
+console.log("I am a regular part of code");
+
+console.log();
+
+// if else 
+if (num1 > num2) {
+  console.log("num 1 is greater than num2");
+}  else {
+    console.log("num2 is greater than num1 ");
+    }
+console.log("I am a regular part of code");
+
+//Check if a string is equal to another String 
+console.log();
+
+let user_name="Chai";
+let another_username="Chai";
+if (user_name==another_username) {
+    console.log("Pick another username");
+    
+}
+
+//Check if a variable is a number or not
+console.log();
+let number = "55";
+if (typeof number=== "number")
+{
+    console.log("Yep ! this is a number");
+}
+else{
+    console.log("No ! This is not a Number");
+    
+}
+// Check if a boolean value is true or False
+isTeaReady=false
+if (isTeaReady) {
+    console.log("Tea is ready");
+    
+} else {
+    console.log("Tea is not Ready");
+    
+}
+
+console.log()
+//To check if anm array is empty or not
+let items=[]
+console.log(items.length);
+
+if (items.length ===0) {
+    console.log("It is empty");
+}
+    else
+{
+    console.log("Array is notb empty");
+    
+}
